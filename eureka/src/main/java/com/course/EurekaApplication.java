b90878d65f17dcae4c25a67f25e9668a896c3ec1
RyanHo <1120177906@qq.com>
2020-12-28 13:06:40 +0800

    Springcloud大型在线视频系统 1.增加eureka子模块作为注册服务端 2.将单maven项目改造成maven父子项目

--- eureka/src/main/java/com/course/EurekaApplication.java
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -1,13 +1,13 @@
-package com.ryan.cloudvi;
+package com.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudviApplication {
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudviApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
