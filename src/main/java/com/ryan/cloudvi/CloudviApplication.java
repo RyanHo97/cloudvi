c07f4c7bdeeb4b7fa87110fc06953249a80cba9a
RyanHo <1120177906@qq.com>
2020-12-28 10:26:23 +0800

    Springcloud大型在线视频系统

---
+++ src/main/java/com/ryan/cloudvi/CloudviApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.cloudvi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryan/cloudvi/CloudviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.cloudvi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
