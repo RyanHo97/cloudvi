9dac8746e93c6cc3bd98231711996ac75fd9b838
RyanHo <1120177906@qq.com>
2020-12-29 18:58:30 +0800

    1.mybatis使用示例：http//127.0.0.1：9001/system/test 测试失败

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,13 +1,21 @@
 package com.course.system.controller;
 
+import com.course.system.domain.Test;
+import com.course.system.service.TestService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class TestController {
 
+    @Resource
+    private TestService testService;
+
     @RequestMapping("/test")
-    public String test(){
-        return "success";
+    public List<Test> test(){
+        return testService.list();
     }
 }
