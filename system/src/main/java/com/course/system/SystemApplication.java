9fef40285d586ed06af876fc60771f51fbbc5ade
RyanHo <1120177906@qq.com>
2020-12-31 10:24:28 +0800

    1.system集成server模块

--- system/src/main/java/com/course/system/SystemApplication.java
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -6,11 +6,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableEurekaClient
-@MapperScan("com.course.system.mapper")
+@ComponentScan(value = "com.course")//加入包扫描路径指定
+@MapperScan("com.course.server.mapper")
 public class SystemApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
