888167535195a816fa08cfb57ed381fc7ee21b4b
RyanHo <1120177906@qq.com>
2020-12-31 11:57:23 +0800

    1.集成了mybatis-generator 步骤1：添加idea插件 步骤2：添加server\src\main\resources\generator\generatorConfig.xml 步骤3：创建maven启动命令mybatis-generator:generate -e

--- server/src/main/java/com/course/server/domain/Test.java
+++ server/src/main/java/com/course/server/domain/Test.java
@@ -1,15 +1,15 @@
 package com.course.server.domain;
 
 public class Test {
-    private int id;
+    private Integer id;
+
     private String name;
 
-
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -19,5 +19,17 @@ public class Test {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append("]");
+        return sb.toString();
     }
 }
--- server/src/main/java/com/course/server/mapper/TestMapper.java
+++ server/src/main/java/com/course/server/mapper/TestMapper.java
@@ -1,10 +1,30 @@
 package com.course.server.mapper;
 
 import com.course.server.domain.Test;
-
+import com.course.server.domain.TestExample;
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface TestMapper {
+    long countByExample(TestExample example);
 
-    public List<Test> list();
+    int deleteByExample(TestExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    List<Test> selectByExample(TestExample example);
+
+    Test selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByPrimaryKeySelective(Test record);
+
+    int updateByPrimaryKey(Test record);
 }
