806641b0b27904685453b880f92f16ce59f94d53
RyanHo <1120177906@qq.com>
2020-12-31 12:35:45 +0800

    1.Example使用示例 使用Example可以快速完成单表的增删改查，熟练掌握后对代码开发效率将有一个质的飞跃

--- server/src/main/java/com/course/server/service/TestService.java
+++ server/src/main/java/com/course/server/service/TestService.java
@@ -1,6 +1,7 @@
 package com.course.server.service;
 
 import com.course.server.domain.Test;
+import com.course.server.domain.TestExample;
 import com.course.server.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,9 @@ public class TestService {
     private TestMapper testMapper;
 
     public List<Test> list(){
-        return testMapper.list();
+        TestExample testExample = new TestExample();
+        testExample.createCriteria().andIdEqualTo(1);
+        testExample.setOrderByClause("id desc");
+        return testMapper.selectByExample(testExample);
     };
 }
