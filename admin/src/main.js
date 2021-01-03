5ce454b2f6f30987247a2564b189fddd99422b73
RyanHo <1120177906@qq.com>
2021-01-03 20:09:40 +0800

    修改了配置

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue'
+import app from './app.vue'
+import router from './router/router'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+new Vue({
+    router,
+    render:h => h(app),
+}).$mount('#app')
