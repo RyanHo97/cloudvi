84b7805aea8963e84fe69ebcc697da56629ed82b
RyanHo <1120177906@qq.com>
2021-01-05 23:14:39 +0800

    1.新增chapter页面及子路由，并使用假数据制作表格

--- admin/src/router/router.js
+++ admin/src/router/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router"
 import Login from "../views/login.vue"
 import Admin from "../views/admin.vue"
 import Welcome from "../views/admin/welcome.vue"
+import Chapter from "../views/admin/chapter.vue"
 
 Vue.use(Router)
 
@@ -21,6 +22,9 @@ export default new Router({
         children:[{
             path:'welcome',
             component:Welcome,
+        }, {
+            path:'chapter',
+            component:Chapter,
         }]
     }]
 })
