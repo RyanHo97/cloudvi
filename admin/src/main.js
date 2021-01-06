8c31cde0e52950388b957ab53fb141ad82d676fc
RyanHo <1120177906@qq.com>
2021-01-06 23:28:15 +0800

    1.集成axios完成前后端交互 2.增加CorsConfig，解决前后端跨域问题 3.解决CorsConfig中出现的由于springboot2.4.0之后产生的问题 .allowedOrigin("*") 更改为 .allowedOriginPatterns("*")

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import app from './app.vue'
 import router from './router/router'
+import axios from 'axios'
 
 Vue.config.productionTip = false;
+Vue.prototype.$ajax=axios;
 
 new Vue({
     router,
     render:h => h(app),
-}).$mount('#app')
+}).$mount('#app');
--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -10,14 +10,14 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/admin/chapter")
 public class ChapterController {
 
     @Resource
     private ChapterService chapterService;
 
-    @RequestMapping("/chapter")
-    public List<ChapterDto> chapter(){
+    @RequestMapping("/list")
+    public List<ChapterDto> list(){
         return chapterService.list();
     }
 }
