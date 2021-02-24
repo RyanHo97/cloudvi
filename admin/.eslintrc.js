e1e12c80e43bacafc4af120084da721249fcafc2
RyanHo <1120177906@qq.com>
2021-02-24 17:12:13 +0800

    1.增加新增大章功能，前端代码开发 2.eslint关闭no-unused-vars校验

--- admin/.eslintrc.js
+++ admin/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
         'vue/no-unused-vars': 'off',
         'vue/require-v-for-key': 'off',
         'no-unused-vars': 'off',
-        'vue/no-unused-components': 'off'
+        'vue/no-unused-components': 'off',
     },
     parserOptions: {
         parser: 'babel-eslint'
