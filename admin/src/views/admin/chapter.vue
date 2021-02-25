08976c7302a1e2978b2f4a3455c67d7d28434166
RyanHo <1120177906@qq.com>
2021-02-25 12:35:06 +0800

    1.集成sweetalert2,删除时弹出确认框

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -136,11 +136,28 @@
 
       del(id){
         let _this =this;
-        _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/'+id).then((response)=>{
-          console.log("删除大章列表结果：",response);
-          let resp = response.data;
-          if (resp.success){
-            _this.list(1);
+        Swal.fire({
+          title: '确认删除？',
+          text: "删除后不可恢复，确认删除？",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: '确认！'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/'+id).then((response)=>{
+              console.log("删除大章列表结果：",response);
+              let resp = response.data;
+              if (resp.success){
+                _this.list(1);
+                Swal.fire(
+                  '删除成功!',
+                  '删除成功！',
+                  'success'
+                )
+              }
+            })
           }
         })
       }
